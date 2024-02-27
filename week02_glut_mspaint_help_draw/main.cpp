f0c428a722abb3bdbd32103f8662ed55efe9ae0b
Hsu06 <112918645+Hsu06@users.noreply.github.com>
2024-02-28 04:37:38 +0800

    Update main.cpp

--- week02_glut_mspaint_help_draw/main.cpp
+++ week02_glut_mspaint_help_draw/main.cpp
@@ -1,12 +1,12 @@
 #include<GL/glut.h>
 void display()
 {
-         glBegin(GL_POLYGON);
+    glBegin(GL_POLYGON);
          glColor3f(1,1,1);
          glVertex2f((30-200)/200.0,-(45-200)/200.0);
          glVertex2f((46-200)/200.0,-(102-200)/200.0);
          glVertex2f((84-200)/200.0,-(23-200)/200.0);
-         glEnd();
+    glEnd();
     glutSwapBuffers();
 }
 int main(int argc,char*argv[])

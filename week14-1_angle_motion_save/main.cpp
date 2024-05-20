646fa4b6b28b00e47bfb8aca39812d50e5120979
Hsu06 <a0916603616@gmail.com>
2024-05-20 17:16:41 +0800

    week14

--- week14-1_angle_motion_save/main.cpp
+++ week14-1_angle_motion_save/main.cpp
@@ -1,3 +1,6 @@
+//#include <stdio.h>
+//FILE * fin = NULL;
+//FILE * fout = NULL;
 #include <opencv/highgui.h> ///使用 OpenCV 2.1 比較簡單, 只要用 High GUI 即可
 #include <opencv/cv.h>
 #include <GL/glut.h>
@@ -16,8 +19,6 @@ int myTexture(char * filename)
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->width, img->height, 0, GL_RGB, GL_UNSIGNED_BYTE, img->imageData);
     return id;
 }
-
-///上週有做過, 請用剪貼的
 #include <GL/glut.h>
 #include "glm.h"
 GLMmodel * pmodel = NULL;
@@ -30,36 +31,29 @@ void drawBody(void)
 {
     if (!body) {
         body = glmReadOBJ("body.obj");
-        ///上週的模型檔,放在奇怪的目錄裡 桌面\freeglut\bin
-        ///本週,要換更好的地方
         if (!body) exit(0);
         glmUnitize(body);
         glmFacetNormals(body);
         glmVertexNormals(body, 90.0);
     }
-
     glmDraw(body, GLM_SMOOTH | GLM_TEXTURE);
 }
 void drawUpper(void)
 {
     if (!upper) {
         upper = glmReadOBJ("upper.obj");
-        ///上週的模型檔,放在奇怪的目錄裡 桌面\freeglut\bin
-        ///本週,要換更好的地方
         if (!upper) exit(0);
         glmUnitize(upper);
         glmFacetNormals(upper);
         glmVertexNormals(upper, 90.0);
     }
-
     glmDraw(upper, GLM_SMOOTH | GLM_TEXTURE);
 }
+
 void drawLower(void)
 {
     if (!lower) {
         lower = glmReadOBJ("lower.obj");
-        ///上週的模型檔,放在奇怪的目錄裡 桌面\freeglut\bin
-        ///本週,要換更好的地方
         if (!lower) exit(0);
         glmUnitize(lower);
         glmFacetNormals(lower);
@@ -67,6 +61,17 @@ void drawLower(void)
     }
 
     glmDraw(lower, GLM_SMOOTH | GLM_TEXTURE);
+}
+void drawmodel(void)
+{
+    if(!pmodel){
+        pmodel = glmReadOBJ("Gundam.obj");
+        if(!pmodel) exit(0);
+        glmUnitize(pmodel);
+        glmFacetNormals(pmodel);
+        glmVertexNormals(pmodel, 90.0);
+    }
+    glmDraw(pmodel, GLM_SMOOTH | GLM_TEXTURE);
 }
 void drawhand1(void)
 {
@@ -97,23 +102,26 @@ void drawhand2(void)
     glmDraw(hand2, GLM_SMOOTH | GLM_TEXTURE);
 }
 int angleID=0;
-float angle[20]={ };
+float angleX[10]={ };
+float angleY[10]={ };
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
-    glutSolidSphere(0.1,30,30);
     glPushMatrix();
          glRotatef(180,0,1,0);
+         glRotatef(angleX[0],1,0,0);
+         glRotatef(angleY[1],0,1,0);
          drawBody();
          glPushMatrix();
              glTranslatef(-0.15,0,0);
-             glRotatef(angle[0],1,0,0);
+             glRotatef(angleX[1],1,0,0);
+             glRotatef(angleY[1],0,1,0);
              glTranslatef(-0.025,-0.05,0);
              drawUpper();
              glPushMatrix();
                 glTranslatef(0,-0.1,0);
-                glRotatef(angle[1],1,0,0);
+                glRotatef(angleX[2],1,0,0);
+                glRotatef(angleY[2],0,1,0);
                 glTranslatef(0,-0.2,0);
                 drawLower();
             glPopMatrix();
@@ -130,11 +138,37 @@ const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
 const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
 const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
 const GLfloat high_shininess[] = { 100.0f };
+
+#include <stdio.h>
+FILE * fout = NULL;
+FILE * fin = NULL;
+
+
 void keyboard(unsigned char key,int x,int y){
-if(key=='0') angleID=0;
-if(key=='1') angleID=1;
-if(key=='2') angleID=2;
-if(key=='3') angleID=3;
+    if(key=='r') {
+        if(fin==NULL) fin = fopen("angle.txt", "r");
+        for(int i=0; i<10; i++) {
+            fscanf(fin, "%f", & angleX[i] );
+            fscanf(fin, "%f", & angleY[i] );
+        }
+        glutPostRedisplay();
+    }
+
+    if(key=='s') {
+        if(fout==NULL) fout = fopen("angle.txt", "w+");
+        for(int i=0; i<10; i++) {
+            printf("%.1f ", angleX[i] );
+            fprintf(fout, "%.1f ", angleX[i] );
+            printf("%.1f ", angleY[i] );
+            fprintf(fout, "%.1f ", angleY[i] );
+        }
+        printf("\n");
+        fprintf(fout, "\n");
+    }
+    if(key=='0') angleID=0;
+    if(key=='1') angleID=1;
+    if(key=='2') angleID=2;
+    if(key=='3') angleID=3;
 }
 int oldX=0,oldY=0;
 void mouse(int button,int state ,int x,int y){
@@ -143,7 +177,8 @@ void mouse(int button,int state ,int x,int y){
 }
 
 void motion(int x,int y){
-    angle[angleID]+=y-oldY;
+    angleX[angleID]+=y-oldY;
+    angleY[angleID]+=x-oldX;
     oldX=x;
     oldY=y;
     glutPostRedisplay();
@@ -158,6 +193,7 @@ int main(int argc, char* argv[])
     glutDisplayFunc(display);
     glutIdleFunc(display);
     glutKeyboardFunc(keyboard);
+    glutMouseFunc(mouse);
     glutMotionFunc(motion);
 
     myTexture("Diffuse.jpg");
@@ -178,5 +214,6 @@ int main(int argc, char* argv[])
     glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
     glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
     glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
     glutMainLoop();
 }
